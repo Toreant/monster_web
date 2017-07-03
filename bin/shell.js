4375a7cbf176a19f6ff8bab95b9a0f443724611c
yuhuajie <18666713785@163.com>
2017-07-03 11:07:40 +0800

    添加测试shell脚本

---
+++ bin/shell.js
@@ -0,0 +1,7 @@
+/**
+ * Created by toreant on 2017/7/3.
+ */
+const fs = require('fs');
+const path = require('path');
+
+fs.appendFile(path.resolve(__dirname, '../logs/shell.txt'), `[shell]${new Date().toLocaleString()}`);
