ca898087854458725f160f7b8091f1a3640d9213
yuhuajie <18666713785@163.com>
2017-07-03 11:13:54 +0800

    添加测试shell脚本

--- bin/shell.js
+++ bin/shell.js
@@ -4,4 +4,4 @@
 const fs = require('fs');
 const path = require('path');
 
-fs.appendFile(path.resolve(__dirname, '../logs/shell.txt'), `[shell]${new Date().toLocaleString()}`);
+fs.appendFile(path.resolve(__dirname, '../logs/shell.txt'), `[shell]${new Date().toLocaleString()}\n`);
